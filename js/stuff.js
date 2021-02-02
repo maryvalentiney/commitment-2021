679e8714f1bf19a8ae0f11483dafb0ebeb752e4e
Mary Yoder <mayo1243@colorado.edu>
2021-02-02 12:03:16 -0700

    arrays

--- js/stuff.js
+++ js/stuff.js
@@ -1,1 +1,20 @@
 console.log("loaded");
+
+var acImages = [
+    "IMG_5875",
+    "IMG_5876",
+    "IMG_5877",
+    "IMG_5878",
+    "IMG_5879",
+    "IMG_5880",
+    "IMG_5881",
+    "IMG_5882",
+    "IMG_5883",
+    "IMG_5884"
+];
+
+var acCaptions = [
+    "Bea, Rodney, and Rosie at this fire that appeared at my plaza randomly the other day",
+    "Taking a break on the beach with Papi while working on my island redesign.",
+    ""
+];
