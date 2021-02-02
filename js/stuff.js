eabb0ccce16595e876bb0d4fef20b4d5bd31be96
Mary Yoder <mayo1243@colorado.edu>
2021-02-02 12:48:30 -0700

    capital I part 4

--- js/stuff.js
+++ js/stuff.js
@@ -24,7 +24,7 @@ var acCaptions = [
     "Papi wears a minecraft shirt during the aurora borealis, this rocks because <span class='eye'>I</span> had been playing minecraft for weeks previously",
     "Built a messed up snowboy because I got fed up did not expect him to get this pissed at me. Recently, <span class='eye'>I</span> have only been making tiny snowboys because they're funnier",
     "<span class='eye'>I</span> demolished this build but it was epic while it lasted <span class='eye'>I</span> will say that",
-    "~~~~~~~~~~museum aesthetic~~~~~~~~~ ALSO <span class='eye'>I</span> AM TWO F<span class='eye'>I</span>SH FROM F<span class='eye'>I</span>N<span class='eye'>I</span>SH<span class='eye'>I</span>NG FISH CRITTERPEDIA!!!!",
+    "~~~~~~~~~~museum aesthetic~~~~~~~~~ ALSO <span class='eye'>I</span> AM TWO F<span class='eye'>I</span>SH FROM F<span class='eye'>I</span>N<span class='eye'>I</span>SH<span class='eye'>I</span>NG F<span class='eye'>I</span>SH CR<span class='eye'>I</span>TTERPED<span class='eye'>I</span>A!!!!",
     "super duper spooky halloween costume feat. Jack, who I think is the same dude that plays Zipper, but he just enjoys the Jack job more"
 
 ];
